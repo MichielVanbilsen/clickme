73e3be13ce97c26ab87186ca043dbb9631e2f788
MichielVanbilsenPXL <11901274@student.pxl.be>
2023-03-25 23:01:23 +0100

    build update

--- src/script.js
+++ src/script.js
@@ -2,16 +2,23 @@ var button = document.getElementById("no");
 
 button.addEventListener("mouseover", function ()
 {
-  console.log("button hovered");
+  move();
+});
 
+button.addEventListener("click", function ()
+{
+  move();
+});
+
+function move()
+{
   var x = Math.floor(Math.random() * window.innerWidth);
   var y = Math.floor(Math.random() * window.innerHeight);
   button.style.top = y + "px";
   button.style.left = x + "px";
-});
+}
 
 var yesbutton = document.getElementById("yes");
-
 yesbutton.addEventListener("click", function ()
 {
   window.location.href = "firework.html";
